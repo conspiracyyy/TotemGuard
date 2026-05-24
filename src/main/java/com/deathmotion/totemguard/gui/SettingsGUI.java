7a3e0312db54252cb350014df1eede4892ef6b15
conspiracy <03grommet-plume@icloud.com>
2026-05-24 12:03:32 +0100

    Update SettingsGUI.java

--- src/main/java/com/deathmotion/totemguard/gui/SettingsGUI.java
+++ src/main/java/com/deathmotion/totemguard/gui/SettingsGUI.java
@@ -11,7 +11,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
-
+// settungtungs
 public class SettingsGUI {
     private final TotemGuard plugin;
     private final GUIManager guiManager;
